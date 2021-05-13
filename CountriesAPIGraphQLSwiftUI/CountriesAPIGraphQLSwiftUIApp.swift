91094c85ef375e90fa5c456155f84866002fe2f3
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-13 13:16:29 +0300

    Initial Commit

---
+++ CountriesAPIGraphQLSwiftUI/CountriesAPIGraphQLSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesAPIGraphQLSwiftUIApp.swift
+//  CountriesAPIGraphQLSwiftUI
+//
+//  Created by ramil on 13.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CountriesAPIGraphQLSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
